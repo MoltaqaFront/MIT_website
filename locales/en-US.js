778a70a08f0a3e2b3d888ec1d17c638da08a67da
shadyaabed <shady@moltaqa.net>
2023-11-26 09:59:07 +0200

    first commit

---
+++ locales/en-US.js
@@ -0,0 +1,65 @@
+export default {
+  lang: 'ltr',
+
+  nav: {
+    home: 'HOME',
+    about: 'ABOUT APP',
+    screen: 'APP SCREENS',
+    download: 'DOWNLOAD NOW',
+    contact: 'CONTACT US',
+    terms: 'TERMS & CONDITIONS',
+    policy: 'Privacy Policy',
+    backToHome: 'BACK TO HOME',
+    lang: 'ع',
+  },
+  registerLink: 'REGISTER AS DRIVER',
+  mainSlider: [
+    {
+      mainTitle: 'About MIT',
+      title: 'The fast and reliable solution for all your needs',
+      desc: 'We are here to meet your needs and make the maintenance and repair process easier and smoother. Let us help you find the right technician for you and meet all your needs with high efficiency and professionalism.',
+    },
+  ],
+  feature: [
+    {
+      image: '1.svg',
+      title: "Qualified technicians",
+      desc: 'Our technicians are carefully selected according to strict standards and have the skills and experience necessary to perform maintenance and repairs accurately and efficiently.',
+    },
+    {
+      image: '2.svg',
+      title: "Diversity of services",
+      desc: 'Providing a comprehensive range of services that include maintenance of computers and mobile phones and application programming'
+    },
+    {
+      image: '3.svg',
+      title: "Ease of use",
+      desc: "The MIT application provides a simple and easy-to-use interface that enables you to request services easily and quickly.",
+    },
+  ],
+  about: {
+    mainTitle: 'MIT app is your ideal choice',
+    title: 'About MIT',
+    desc: "<p>A multi-service online store application that allows users to purchase products and services online and have them delivered to their address. The application also includes reservation options for restaurants and events, and facilitates the process of obtaining the required services in an easy and convenient way. Those who are looking for a modern and sophisticated shopping experience, as it provides many distinctive and convenient services that make online shopping easy and enjoyable. Therefore, the application can be downloaded easily and benefit from all the services available on it with comfort and confidence. Get the best online shopping experience with the Pring application. Enjoy competitive prices and high quality of the products you buy through the MIT app. Fast and reliable delivery for all your purchases, with secure and easy payment options</p>",
+  },
+  downloadText: "<p>You can download the MIT app now by visiting the App Store on your smartphone, searching for MIT, and then downloading the app on your smartphone.</p>",
+  isoLink: "",
+  androidLink: "",
+  address: 'KSA - Jeddah',
+  terms: {
+    title: 'Terms And Conditions',
+  },
+  policy: {
+    title: 'Privacy Policy',
+  },
+  copyWrite: '©2023 All Rights Reserved For Moltaqa Software House',
+
+  contact: {
+    title: 'Contact Us',
+    name: 'name',
+    email: 'e-mail',
+    phone: 'phone',
+    message: 'message',
+    send: 'send ',
+    },
+}
